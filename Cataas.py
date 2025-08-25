838abda5667352dd0d03a14b873c59ccdbf24ac8
pythonalba-lang <albin4ickblin4ick@mail.ru>
2025-08-25 20:37:17 +0300

    Создано окно и виджеты

---
+++ Cataas.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+def load_image(url):
+    try:
+        # Отправляем GET-запрос с использованием requests.get()
+        response = requests.get(url)
+
+        # Проверяем успешность запроса (код ответа 200)
+        response.raise_for_status()
+
+        # Читаем байты из ответа в объект BytesIO
+        image_data = BytesIO(response.content)
+
+        # Открываем изображение с помощью PIL
+        img = Image.open(image_data)
+
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+        return None
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+# Создаем метку без изображения
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    # Устанавливаем изображение в метку
+    label.config(image=img)
+    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
+    label.image = img
+
+window.mainloop()
