c352e99e336cea8ad8195811f40b0f489d8f2b63
pythonalba-lang <albin4ickblin4ick@mail.ru>
2025-08-26 10:40:42 +0300

    добавили меню

--- Cataas.py
+++ Cataas.py
@@ -7,36 +7,65 @@ def load_image(url):
     try:
         # Отправляем GET-запрос с использованием requests.get()
         response = requests.get(url)
-
         # Проверяем успешность запроса (код ответа 200)
         response.raise_for_status()
-
         # Читаем байты из ответа в объект BytesIO
         image_data = BytesIO(response.content)
-
         # Открываем изображение с помощью PIL
         img = Image.open(image_data)
+        # Изменяем размер изображения
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
 
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Ошибка при загрузке изображения: {e}")
         return None
 
+
+def set_image():
+    # Вызываем функцию для загрузки изображения
+    img = load_image(url)
+
+    if img:
+        # Устанавливаем изображение в метку
+        label.config(image=img)
+        label.image = img # Сохраняем ссылку на изображение
+
+def exit():
+    window.destroy()
+
+
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
+window.geometry("600x520")
 
 # Создаем метку без изображения
-label = Label()
+label = Label(window)
 label.pack()
 
-url = 'https://cataas.com/cat'
-img = load_image(url)
+# update_button = Button(text="Обновить", command=set_image)
+# update_button.pack()
+
+# Создаем меню
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+# Добавляем пункты меню
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
+
+url = url = 'https://cataas.com/cat/cute'
+
 
 if img:
     # Устанавливаем изображение в метку
     label.config(image=img)
     # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
     label.image = img
+
+set_image()
 
 window.mainloop()
